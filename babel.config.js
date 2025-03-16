d8b80acc90fcb4a401947dcae8e582ce646e6db8
Dragan Stojanovski <dragan.stojanovski@outlook.com>
2025-03-17 00:05:33 +1100

    Added React Native Reanimated

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,13 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: ["react-native-reanimated/plugin"],
     env: {
       production: {
-        plugins: ["react-native-paper/babel"],
+        plugins: [
+          "@babel/plugin-proposal-export-namespace-from",
+          "react-native-paper/babel",
+        ],
       },
     },
   };

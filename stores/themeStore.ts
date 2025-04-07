e0fbd015e2d842a756262dca764415e46391119d
Dragan Stojanovski <dragan.stojanovski@outlook.com>
2025-04-07 17:57:05 +1000

    Created purple theme

--- stores/themeStore.ts
+++ stores/themeStore.ts
@@ -1,8 +1,9 @@
 import { create } from "zustand";
 import { blueLight, blueDark } from "@/themes/blue-theme";
+import { purpleLight, purpleDark } from "@/themes/purple-theme";
 import { Appearance } from "react-native";
 
-type Theme = typeof blueLight;
+type Theme = typeof purpleLight;
 
 interface ThemeState {
   colorScheme: "light" | "dark";
@@ -14,10 +15,10 @@ const initialColorScheme = Appearance.getColorScheme() || "light";
 
 export const useThemeStore = create<ThemeState>((set) => ({
   colorScheme: initialColorScheme,
-  theme: initialColorScheme === "dark" ? blueDark : blueLight,
+  theme: initialColorScheme === "dark" ? purpleDark : purpleLight,
   setTheme: (colorScheme: "light" | "dark") =>
     set({
       colorScheme,
-      theme: colorScheme === "dark" ? blueDark : blueLight,
+      theme: colorScheme === "dark" ? purpleDark : purpleLight,
     }),
 }));

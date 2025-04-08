de4ccfafcec48fa056c9646f50165042e9efd219
Dragan Stojanovski <dragan.stojanovski@outlook.com>
2025-04-08 21:22:34 +1000

    Fixed View component color prop

--- components/ui/View.tsx
+++ components/ui/View.tsx
@@ -3,8 +3,6 @@ import { useThemeStore } from "@/stores/themeStore";
 
 export default function MyView({ style, ...props }: ViewProps) {
   const theme = useThemeStore((state) => state.theme);
-  const { colors } = theme;
-  return (
-    <View {...props} style={[{ backgroundColor: colors.background }, style]} />
-  );
+  const color = theme.colors.background;
+  return <View {...props} style={[{ backgroundColor: color }, style]} />;
 }

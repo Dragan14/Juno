ea453a741de515e30f9ff85f3fb0a1b9fb8a301b
Dragan Stojanovski <dragan.stojanovski@outlook.com>
2025-04-09 19:39:48 +1000

    Added link prop to Text component

--- components/ui/Text.tsx
+++ components/ui/Text.tsx
@@ -1,4 +1,5 @@
-import { Text, TextProps, StyleProp, TextStyle } from "react-native";
+import React, { forwardRef, ForwardedRef } from "react";
+import { Text, TextProps, StyleProp, TextStyle, Pressable } from "react-native";
 import { useThemeStore } from "@/stores/themeStore";
 
 type MyTextProps = {
@@ -12,39 +13,59 @@ type MyTextProps = {
     | "error"
     | "success";
   color?: string;
+  link?: boolean;
 } & TextProps;
 
-export default function MyText({
-  children,
-  style,
-  variant,
-  color,
-  ...props
-}: MyTextProps) {
-  const theme = useThemeStore((state) => state.theme);
+const MyText = forwardRef(
+  (
+    { children, style, variant, color, link, onPress, ...props }: MyTextProps,
+    ref: ForwardedRef<Text>,
+  ) => {
+    const theme = useThemeStore((state) => state.theme);
 
-  color = (() => {
-    switch (variant) {
-      case "default":
-        return theme.colors.onSurface;
-      case "primary":
-        return theme.colors.onPrimary;
-      case "secondary":
-        return theme.colors.onSecondary;
-      case "tertiary":
-        return theme.colors.onTertiary;
-      case "error":
-        return theme.colors.onError;
-      case "success":
-        return theme.colors.onSuccess;
-      default:
-        return color ?? theme.colors.onSurface;
-    }
-  })();
+    color = (() => {
+      switch (variant) {
+        case "default":
+          return theme.colors.onSurface;
+        case "primary":
+          return theme.colors.onPrimary;
+        case "secondary":
+          return theme.colors.onSecondary;
+        case "tertiary":
+          return theme.colors.onTertiary;
+        case "error":
+          return theme.colors.onError;
+        case "success":
+          return theme.colors.onSuccess;
+        default:
+          return color ?? theme.colors.onSurface;
+      }
+    })();
 
-  return (
-    <Text style={[{ color: color }, style]} {...props}>
-      {children}
-    </Text>
-  );
-}
+    return link ? (
+      <Pressable onPress={onPress}>
+        {({ pressed, hovered }) => (
+          <Text
+            ref={ref}
+            style={[
+              { color: color },
+              pressed || hovered ? { textDecorationLine: "underline" } : {},
+              style,
+            ]}
+            {...props}
+          >
+            {children}
+          </Text>
+        )}
+      </Pressable>
+    ) : (
+      <Text ref={ref} style={[{ color: color }, style]} {...props}>
+        {children}
+      </Text>
+    );
+  },
+);
+
+MyText.displayName = "Text";
+
+export default MyText;

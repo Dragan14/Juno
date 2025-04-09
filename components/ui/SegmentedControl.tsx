e8c15239f73438f3e2f20d2942afb6fdcd9bf313
Dragan Stojanovski <dragan.stojanovski@outlook.com>
2025-04-09 18:52:36 +1000

    Added round prop to SegmentedControl

--- components/ui/SegmentedControl.tsx
+++ components/ui/SegmentedControl.tsx
@@ -3,7 +3,9 @@ import { PixelRatio } from "react-native";
 import SegmentedControlTab from "react-native-segmented-control-tab";
 import { useThemeStore } from "@/stores/themeStore";
 
-type SegmentedControlProps = ComponentProps<typeof SegmentedControlTab>;
+type SegmentedControlProps = {
+  round?: boolean;
+} & ComponentProps<typeof SegmentedControlTab>;
 
 export default function SegmentedControl({
   selectedIndex = 0,
@@ -12,7 +14,8 @@ export default function SegmentedControl({
   tabTextStyle,
   activeTabTextStyle,
   borderRadius,
-  allowFontScaling,
+  allowFontScaling = true,
+  round = false,
   ...props
 }: SegmentedControlProps) {
   const { theme } = useThemeStore();
@@ -20,16 +23,28 @@ export default function SegmentedControl({
   return (
     <SegmentedControlTab
       selectedIndex={selectedIndex}
-      tabStyle={{
-        backgroundColor: theme.colors.background,
-        borderColor: theme.colors.outline,
-        height: scaledSize(40),
-      }}
-      activeTabStyle={{ backgroundColor: theme.colors.secondaryContainer }}
-      tabTextStyle={{ color: theme.colors.onBackground, fontWeight: "500" }}
-      activeTabTextStyle={{ color: theme.colors.onSecondaryContainer }}
-      borderRadius={20}
-      allowFontScaling={true}
+      tabStyle={[
+        tabStyle,
+        {
+          backgroundColor: theme.colors.background,
+          borderColor: theme.colors.outline,
+          height: scaledSize(40),
+        },
+      ]}
+      activeTabStyle={[
+        activeTabStyle,
+        { backgroundColor: theme.colors.secondaryContainer },
+      ]}
+      tabTextStyle={[
+        tabTextStyle,
+        { color: theme.colors.onBackground, fontWeight: "500" },
+      ]}
+      activeTabTextStyle={[
+        activeTabTextStyle,
+        { color: theme.colors.onSecondaryContainer },
+      ]}
+      borderRadius={borderRadius ?? (round ? scaledSize(20) : scaledSize(5))}
+      allowFontScaling={allowFontScaling}
       {...props}
     />
   );
